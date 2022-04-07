2a606e2df1f35793e5341b43c0d27a1dba5c5a8c
Mikhail Karasik <mikhail.karasik@sectigo.com>
2022-04-07 09:47:56 -0400

    first commit

---
+++ project/src/main/java/com/example/okhttptest/OkhttpTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.okhttptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkhttpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkhttpTestApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/example/okhttptest/OkhttpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okhttptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkhttpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
